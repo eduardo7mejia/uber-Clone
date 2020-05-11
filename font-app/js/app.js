69368c98da0eee605132b7c771118ecf2c1556fd
eduardo7mejia <mejiaeduardo144@gmail.com>
2020-05-11 18:43:17 -0500

    version 2

--- font-app/js/app.js
+++ font-app/js/app.js
@@ -1,6 +1,32 @@
-console.log("Hola mundo");
-
-fetch("https://randomuser.me/api/?results=2")
+fetch("https://randomuser.me/api/?results=12")
 .then((resp)=>resp.json())
-.then((data)=>console.log(data))
-.catch((error)=>console.log(error));
+.then((data)=>{
+    let contenedorUsuarios = document.getElementById("contenedorUsuarios");
+    let usuarios = data.results;
+    // console.log(contenedorUsuarios);
+    // console.log(usuarios)
+
+    // for(let i = 0; i < usuarios.length ; i++){
+    //     console.log(usuarios[i]);
+    // }
+    // for (const key in usuarios) {
+    //     console.log(usuarios[key])
+    // }
+
+    contenedorUsuarios.innerHTML = "";
+    usuarios.forEach((usuario,indice) => {
+        contenedorUsuarios.innerHTML += `<div class="col-xl-3 col-lg-4 col-md-4 col-sm-6">
+            <div class="card mb-3 shadow text-uppercase">
+                <img src="${usuario.picture.large}" class="card-img-top" alt="cover-${indice+1}">
+                <div class="card-body">
+                    <h5 class="card-title">${usuario.name.title}. ${usuario.name.first} ${usuario.name.last} </h5>
+                    <p class="card-text">Ciudad: ${usuario.location.city}</p>
+                    <a href="#" class="btn btn-primary ">ver Perfil</a>
+                </div>
+            </div>
+        </div>`;
+        console.log(usuario);
+    });
+
+})
+.catch((error)=>console.error(error));
